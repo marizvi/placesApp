7d0c0223ff7e48565de1e05dcc78c9defeb6b5c0
marizvi <202051121@iiitvadodara.ac.in>
2022-01-20 01:57:30 +0530

    address working

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -79,4 +79,8 @@ class GreatPlaces with ChangeNotifier {
         .toList();
     notifyListeners();
   }
+
+  Place findById(String id) {
+    return _items.firstWhere((element) => element.id == id);
+  }
 }
